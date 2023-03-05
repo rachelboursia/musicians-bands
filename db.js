2883ec9d6f42028d1efa59329c595669b76144bc
rachelboursia <rachel.l.boursia@gmail.com>
2023-03-05 12:34:24 -0800

    new sequlize connection

--- db.js
+++ db.js
@@ -2,6 +2,10 @@ const path = require('path');
 const { Sequelize, Model } = require('sequelize');
 
 // TODO - create the new sequelize connection
+const sequelize = new Sequelize({
+    dialect: 'splite',
+    storage: path.join(__dirname, 'db.splite')
+});
 
 module.exports = {
     sequelize,
